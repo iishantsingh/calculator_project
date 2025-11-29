6f49a5ed05255c5800e988d2f30df24ef979f3fe
iishantsingh <ishantbadgujjar26@gmail.com>
2025-11-30 01:03:34 +0530

    Add files via upload

---
+++ calculator_project/calculator.py
@@ -0,0 +1,39 @@
+# Simple Calculator Project
+# Created by Ishant Singh
+# Roll Number: 2501410067
+# B.Tech CSE (Cybersecurity), Semester 1
+
+print("Simple Calculator")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Choose operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+if choice == '1':
+    result = num1 + num2
+    print("Result:", result)
+
+elif choice == '2':
+    result = num1 - num2
+    print("Result:", result)
+
+elif choice == '3':
+    result = num1 * num2
+    print("Result:", result)
+
+elif choice == '4':
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Cannot divide by zero")
+
+else:
+    print("Invalid Input")
